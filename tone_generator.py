dadb65b31bef6220ed1fac421b8b5c7d39df1d67
harrysentonbury <52933214+harrysentonbury@users.noreply.github.com>
2019-11-11 20:18:47 +0000

    see what happens when idrag a file in

---
+++ tone_generator.py
@@ -0,0 +1,33 @@
+
+import argparse
+import numpy as np
+import sounddevice as sd
+import time
+
+
+def play(frequency=440, duration=1.5):
+    sample_rate = 44100
+
+    x = np.linspace(0, duration, duration * sample_rate)
+    wave = np.sin(2 * np.pi * x * frequency) * 0.3
+
+    sd.play(wave, sample_rate)
+    time.sleep(duration)
+    sd.stop()
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("frequency", help="The required frequency in hertz", type=float)
+    parser.add_argument("duration", nargs='?',
+                        help="Required duration in seconds", type=float)
+    args = parser.parse_args()
+
+    if args.duration:
+        play(args.frequency, args.duration)
+    else:
+        play(args.frequency)
+
+
+if __name__ == '__main__':
+    main()
