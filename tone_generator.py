32cbc744d45a57d0b5ed00a87cf109eaa81d6b50
harrysentonbury <harrysentonbury@gmail.com>
2020-01-15 13:54:05 +0000

    fixed error

--- tone_generator.py
+++ tone_generator.py
@@ -8,7 +8,7 @@ import time
 def play(frequency=440, duration=1.5):
     sample_rate = 44100
 
-    x = np.linspace(0, duration, duration * sample_rate)
+    x = np.linspace(0, duration, int(duration * sample_rate))
     wave = np.sin(2 * np.pi * x * frequency) * 0.3
 
     sd.play(wave, sample_rate)
@@ -30,6 +30,10 @@ def main():
             play(args.frequency)
     except ValueError:
         return play(args.frequency)
+    except KeyboardInterrupt:
+        parser.exit("\n Interrupt by user")
+    except Exception as e:
+        parser.exit(type(e).__name__ + ': ' + str(e))
 
 
 if __name__ == '__main__':
