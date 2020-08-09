f19a6e5415f81fa38d46b63a3e5f209c2aee5071
harrysentonbury <harrysentonbury@gmail.com>
2020-08-09 14:24:51 +0100

    error handling

--- tone_generator.py
+++ tone_generator.py
@@ -8,35 +8,38 @@ import time
 
 def play(frequency=440, duration=1.5):
     sample_rate = 48000
+    if frequency <= 0:
+        print('must be positive numbers')
+        parser.print_help()
+    if duration <= 0:
+        print('must be positive numbers')
+        parser.print_help()
+    else:
+        x = np.linspace(0, duration, int(duration * sample_rate))
+        wave = np.sin(2 * np.pi * x * frequency) * 0.3
 
-    x = np.linspace(0, duration, int(duration * sample_rate))
-    wave = np.sin(2 * np.pi * x * frequency) * 0.3
-
-    sd.play(wave, sample_rate)
-    time.sleep(duration)
-    sd.stop()
-
-
-def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "frequency", help="required frequency in hertz", type=float)
-    parser.add_argument("duration", nargs="?",
-                        help="required duration in seconds", type=float)
-    args = parser.parse_args()
-
-    try:
-        if args.duration:
-            play(args.frequency, args.duration)
-        else:
-            play(args.frequancy)
-    except ValueError:
-        return play(args.frequancy)
-    except KeyboardInterrupt:
-        parser.exit("\n Interupt by user")
-    except Exception as e:
-        parser.exit(type(e).__name__ + ': ' + str(e))
+        sd.play(wave, sample_rate)
+        time.sleep(duration)
+        sd.stop()
 
 
-if __name__ == '__main__':
-    main()
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "frequency", nargs='?', help="required frequency in hertz", type=float)
+parser.add_argument("duration", nargs="?",
+                    help="required duration in seconds", type=float)
+args = parser.parse_args()
+
+try:
+    if args.duration:
+        play(args.frequency, args.duration)
+    elif args.frequency:
+        play(args.frequency)
+    else:
+        play()
+except ValueError:
+    play(args.frequency)
+except KeyboardInterrupt:
+    parser.exit("\n Interupt by user")
+except Exception as e:
+    parser.exit(type(e).__name__ + ': ' + str(e))
